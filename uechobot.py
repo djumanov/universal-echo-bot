beb7545702a1d71f9cb9fa95784751d32d552abf
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-16 17:39:50 +0500

    added get last update function

---
+++ uechobot.py
@@ -0,0 +1,14 @@
+import requests
+
+TOKEN = '2052462740:AAF5wRpqoXt2bm2a7JK5PlFPmcDLRGlajr0'
+
+def get_last_update() -> dict:
+    url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+    r = requests.get(url=url)
+    if r.status_code == 200:
+        last_update = r.json()['result'][-1]
+        return last_update
+    return False
+
+print(get_last_update())
+        
