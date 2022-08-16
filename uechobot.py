5d557fa95d12c0ad788dc7ad5d88af0bf0f01fda
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-16 17:43:18 +0500

    added docstring into get last update function

--- uechobot.py
+++ uechobot.py
@@ -3,6 +3,8 @@ import requests
 TOKEN = '2052462740:AAF5wRpqoXt2bm2a7JK5PlFPmcDLRGlajr0'
 
 def get_last_update() -> dict:
+    '''this function returns last update as dictionary from telegram'''
+
     url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
     r = requests.get(url=url)
     if r.status_code == 200:
@@ -10,5 +12,3 @@ def get_last_update() -> dict:
         return last_update
     return False
 
-print(get_last_update())
-        
